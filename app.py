90869bc239e8300b986d16c57f952b953dcf69e6
emgen <49552259+emgen@users.noreply.github.com>
2021-03-08 14:57:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,27 @@
+import pandas as pd
+import plotly.express as px
+
 import dash
+import dash_table
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly.graph_objs as go
-from dash.dependencies import Input, Output, State
+from dash.dependencies import Input, Output
 
-########### Define your variables ######
+########### Define your variables
 
-myheading1='How to use callbacks with multiple inputs'
-tabtitle = 'lightsabers!'
-list_of_colors=['red', 'green', 'blue']
-list_of_numbers=['one', 'two', 'three']
-sourceurl = 'https://dash.plot.ly/getting-started-part-2'
-githublink = 'https://github.com/austinlasseter/dash-callbacks-multi-input'
+cust_dff= pd.read_csv("Cust_df.csv")
+
+########### Set up the chart
 
 
-########## Set up the chart
+fig = px.line(
+    y= cust_dff['Customer Count'] , # name used in legend and hover labels
+    x=cust_dff['Date'],labels={'y':'No. Customers','x':"Date" })
+
+fig.update_layout(
+    title_text='Active Customer Report', # title of plot
+    xaxis_tickangle=-45,xaxis_title_text='Date', 
+    yaxis_title_text='Number of Customers')
 
 ########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -25,50 +32,13 @@ app.title=tabtitle
 ########### Set up the layout
 
 app.layout = html.Div(children=[
-    html.H1(myheading1),
-    html.Div([
-        html.Div([
-            dcc.RadioItems(
-                id='pick-a-color',
-                options=[
-                        {'label':list_of_colors[0], 'value':list_of_colors[0]},
-                        {'label':list_of_colors[1], 'value':list_of_colors[1]},
-                        {'label':list_of_colors[2], 'value':list_of_colors[2]},
-                        ],
-                value='choose',
-                ),
-        ],className='two columns'),
-        html.Div([
-            dcc.RadioItems(
-                id='pick-a-number',
-                options=[
-                        {'label':list_of_numbers[0], 'value':list_of_numbers[0]},
-                        {'label':list_of_numbers[1], 'value':list_of_numbers[1]},
-                        {'label':list_of_numbers[2], 'value':list_of_numbers[2]},
-                        ],
-                value='one',
-                ),
-        ],className='two columns'),
-        html.Div([
-            html.Div(id='your_output_here', children=''),
-        ],className='eight columns'),
-    ],className='twelve columns'),
-    html.Br(),
-    html.A('Code on Github', href=githublink),
-    html.Br(),
-    html.A("Data Source", href=sourceurl),
+    html.H1(myheading),
+    dcc.Graph(
+        id='flyingdog',
+        figure= fig
+    ),
     ]
 )
 
-########## Define Callback
-
-@app.callback(Output('your_output_here', 'children'),
-              [Input('pick-a-color', 'value'),
-               Input('pick-a-number', 'value')])
-def radio_results(color_you_picked, number_you_picked):
-    image_you_chose=f'{color_you_picked}-{number_you_picked}.jpg'
-    return html.Img(src=app.get_asset_url(image_you_chose), style={'width': 'auto', 'height': 'auto'}),
-
-############ Deploy
 if __name__ == '__main__':
     app.run_server()
